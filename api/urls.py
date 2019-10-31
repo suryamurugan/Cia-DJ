6aa2fc60da95475366c4905110f042ea7226acaa
root <root@ciadb.localdomain>
2019-10-31 08:43:15 +0000

    Added

--- api/urls.py
+++ api/urls.py
@@ -15,15 +15,18 @@ urlpatterns = [
   path('attend/', attend), # FOR ATTENDANCE
   path('getstats/',getstats), # FOR GETTING STATS
   path('getProjects/',ProjectView.as_view()), # GET PROJECTS 
-  path('users/', CustomRegisterView.as_view(),name="something"),  #DJANGO USER
-  path('rest-auth/', include('rest_auth.urls')), # ALL REST AUTH URLS
-  url(r'rest-auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(),name='account_confirm_email'),
-  path('rest-auth/registration/', include('rest_auth.registration.urls')), #REST USER
-  path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # <-- And here
-  path('events/', ListEventsView.as_view(), name='getevents'),  # <-- And here
+   path('events/', ListEventsView.as_view(), name='getevents'),  # <-- And here
   path('news/', ListNewsView.as_view(), name='getnews'),  # <-- And here
-  url(r'^login/$', LoginUserDetailView.as_view(), name='rest_login'),
-  url(r'^custom/login/$', CustomLoginView.as_view(), name='rest_login'),
-  url(r'^custom/register/$', VeryNewCustomRegisterView.as_view(), name='rest_register'),
-  path(r'^*account-confirm-email/(?P<key>[-:\w]+)/$', TemplateView.as_view(), name='account_confirm_email'),  # <-- And here
+  #path('users/', CustomRegisterView.as_view(),name="something"),  #DJANGO USER
+  path('rest-auth/', include('rest_auth.urls')), # ALL REST AUTH URLS
+
+
+  #url(r'rest-auth/registration/account-confirm-email/(?P<key>[-:\w]+)/$', ConfirmEmailView.as_view(),name='account_confirm_email'),
+  #path('rest-auth/registration/', include('rest_auth.registration.urls')), #REST USER
+  #path('api-token-auth/', obtain_auth_token, name='api_token_auth'),  # <-- And here
+ 
+  # just token - url(r'^login/$', LoginUserDetailView.as_view(), name='rest_login'),
+  
+ #REDUNDANT url(r'^custom/register/$', VeryNewCustomRegisterView.as_view(), name='rest_register'),
+ # path(r'^*account-confirm-email/(?P<key>[-:\w]+)/$', TemplateView.as_view(), name='account_confirm_email'),  # <-- And here
   ]
--- cia/urls.py
+++ cia/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,re_path, include
 from django.conf.urls import url
 
 from allauth.account.views import confirm_email
-from api.views import attend,something,getUser,activate,VeryNewCustomRegisterView,testReset
+from api.views import attend,something,getUser,activate,VeryNewCustomRegisterView,testReset,CustomLoginView
 
 from django.views.generic import TemplateView
 from django.contrib.auth.views import password_reset_confirm,password_reset_complete
@@ -26,17 +26,19 @@ from django.contrib.auth.views import password_reset_confirm,password_reset_comp
 from api.views import HelloView
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('act/<uid>/<token>/',something), 
-    path('getUser/',getUser),
-    path('hello/', HelloView.as_view(), name='hello'),
-    re_path('api/(?P<version>(v1|v2))/', include('api.urls')),
-    url(r'^act/<uidb64>/<token>/$', activate, name='activate'),
-    url(r'register/$', VeryNewCustomRegisterView.as_view(), name='rest_register'),
-    url(r'^account_inactive/$',TemplateView.as_view(),name = 'account_inactive'),
-    url(r'reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, name='password_reset_confirm'),
+    url(r'reset/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, name='password_reset_confirm'), # POST RESET MAIL 
     url(r'reset/confirm/done/$', password_reset_complete, name='password_reset_complete'),
-    path('reset-password/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-    path('testreset/',testReset,name='test_reset'),
+    re_path('api/(?P<version>(v1|v2))/', include('api.urls')),
+    url(r'register/$', VeryNewCustomRegisterView.as_view(), name='rest_register'),
+    url(r'^login/$', CustomLoginView.as_view(), name='rest_login'),
+    url(r'^account_inactive/$',TemplateView.as_view(),name = 'account_inactive'), # no idea y but register needs this
+    path('activate/<uid>/<token>/',something),     # ACTIVATEING ACCOUNT - LINK MAILED 
+
+    #url(r'^act/<uidb64>/<token>/$', activate, name='activate'),
+    #path('hello/', HelloView.as_view(), name='hello'),
+    #path('getUser/',getUser),
+    #path('reset-password/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    path('password-reset/',testReset,name='password_reset'), #CUSTOM RESET PAGE
 
 
 ]
